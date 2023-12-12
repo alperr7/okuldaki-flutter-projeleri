9c8d270759d555412e80f911cd8dc2fe9af09709
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-12 19:27:13 +0300

    l

---
+++ Flutter/w06derstekrari/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:w06derstekrari/01_noncontainer_widgets.dart';
+import 'package:w06derstekrari/02_container_widgets.dart';
+
+void main(List<String> args) {
+  runApp(Sayfa2());
+}
