70ba62826348ddc8f5b80b15c14b41bb08f853db
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2024-01-10 14:58:05 +0300

    l

--- Flutter/w11derstekrari/lib/navigasyon1.dart
+++ Flutter/w11derstekrari/lib/navigasyon1.dart
@@ -7,10 +7,7 @@ class Navigasyon1MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Home",
-          style: TextStyle(color: Colors.white),
-        ),
+        title: Text("Home", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.deepPurple,
       ),
       body: Center(
@@ -23,10 +20,9 @@ class Navigasyon1MainPage extends StatelessWidget {
                 },
               ));
 
-              print(responsedData.toString() + ": Retrived");
-
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(responsedData)));
+              print(Navigasyon1Page1.responsedData.toString() + ": Retrived");
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(Navigasyon1Page1.responsedData.toString())));
             },
             child: Text("Goto page 1")),
       ),
@@ -37,28 +33,59 @@ class Navigasyon1MainPage extends StatelessWidget {
 class Navigasyon1Page1 extends StatelessWidget {
   int userId = 15;
   Navigasyon1Page1(this.userId);
+  static String responsedData = "";
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () {},
+      onWillPop: () => checkStatus(context),
       child: Scaffold(
         appBar: AppBar(
           title: Text(
             "Page 1, userid = $userId",
             style: TextStyle(color: Colors.white),
           ),
+          automaticallyImplyLeading: false,
           backgroundColor: Colors.deepPurple,
         ),
         body: Center(
           child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context, "Kullanıcı bligileri güncellendi");
+            onPressed: () async {
+              //Navigator.pop(context, "Kullanıcı bligileri güncellendi");
+              var isOk = await checkStatus(context);
+              if (isOk) {
+                Navigator.pop(context, "Kullanıcı bilgileri güncellendi");
+              }
             },
             child: Text("<<Back"),
           ),
         ),
       ),
     );
+  }
+
+  Future<bool> checkStatus(BuildContext context) async {
+    var shouldPop = await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Bilgileri eksiksiz girdiniz mi?"),
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                  responsedData = "Kullanıcı bilgileri güncellendi";
+                },
+                child: Text("Evet")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: Text("Hayır")),
+          ],
+        );
+      },
+    );
+    return shouldPop;
   }
 }
